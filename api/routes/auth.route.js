073a49dd4544e7eb4dff86549ddbaa6267181b75
Emma Sargsyan <emmasargsyan39@gmail.com>
2024-05-09 21:20:07 +0300

    Google Auth

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import {signup} from '../controllers/auth.controller.js';
-import {sign} from '../controllers/auth.controller.js';
+import {sign,signout,google } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/sign", sign);
+router.get('/signout',signout)
+router.post('/google',google)
+
 
 export default router;
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  authDomain: "mern-auth-522a1.firebaseapp.com",
+  projectId: "mern-auth-522a1",
+  storageBucket: "mern-auth-522a1.appspot.com",
+  messagingSenderId: "1077419438886",
+  appId: "1:1077419438886:web:24cea5c86c6c54af63c7e7"
 };
 
 // Initialize Firebase
